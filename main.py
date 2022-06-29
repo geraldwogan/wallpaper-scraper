c7cc9e56e83f0773944b7d3844c90cb4948d87c8
geraldwogan <geraldwogan@gmail.com>
2022-06-30 00:37:48 +0100

    Add relative path for image using pathlib

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import ctypes
+from pathlib import Path
 
 def set_wallpaper(image_path):
     ctypes.windll.user32.SystemParametersInfoW(20, 0, image_path , 0)
 
 def main():
-    set_wallpaper("C:\\Users\\Gerald\\Documents\\Personal\\Coding\\Projects\\wallpaper-scraper\\images\\darjeeling.jpg")
+    image_name = "greenbluebg.jpg"
+    image_path = Path.cwd() / 'wallpaper-scraper' / 'images' / image_name
+    set_wallpaper(str(image_path))
 
 if __name__ == '__main__':
     main()
