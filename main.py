940ff947894b1fe50f9d7eb9ce3d253959eb2ef8
geraldwogan <geraldwogan@gmail.com>
2022-07-01 23:56:34 +0100

    Initial attempt to use beautifulsoup for scraping images

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 import ctypes
+from bs4 import BeautifulSoup
 from pathlib import Path
+from urllib.request import urlopen
 
 def set_wallpaper(image_path):
     ctypes.windll.user32.SystemParametersInfoW(20, 0, image_path , 0)
 
+def get_image_link():
+    htmldata = urlopen("https://www.reddit.com/r/art/top/?t=day")
+    soup = BeautifulSoup(htmldata, 'html.parser')
+    images = soup.find_all('img') # data-url="https://i.redd.it/l9gjoxwomz891.jpg"
+    
+    for item in images:
+        print(item['src'])
+
+
 def main():
-    image_name = "greenbluebg.jpg"
-    image_path = Path.cwd() / 'wallpaper-scraper' / 'images' / image_name
-    set_wallpaper(str(image_path))
+    # image_name = "greenbluebg.jpg"
+    # image_path = Path.cwd() / 'wallpaper-scraper' / 'images' / image_name
+    # set_wallpaper(str(image_path))
+    get_image_link()
 
 if __name__ == '__main__':
     main()
+
+
